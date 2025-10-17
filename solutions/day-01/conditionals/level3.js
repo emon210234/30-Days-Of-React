3e5bcbafaa230010e1a3376f08dc982c37435a56
emon210234 <emon2134@cseku.ac.bd>
2025-10-17 19:10:49 +0600

    chore: daily_exercises_conditionals_level_03: Completed

---
+++ solutions/day-01/conditionals/level3.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')({sight: true});
+
+let month = prompt("Enter the month:").toLowerCase();
+
+switch (month) {
+  case "january":
+  case "march":
+  case "may":
+  case "july":
+  case "august":
+  case "october":
+  case "december":
+    console.log(`${month} has 31 days.`);
+    break;
+
+  case "april":
+  case "june":
+  case "september":
+  case "november":
+    console.log(`${month} has 30 days.`);
+    break;
+
+  case "february":
+    console.log(`${month} has 28 days, or 29 in a leap year.`);
+    break;
+
+  default:
+    console.log("Invalid month.");
+}
